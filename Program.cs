f46c8f1c2b0a03d9c21ae3dea5352ef3531f3928
riyandabella <riyandabella561@gmail.com>
2025-03-10 15:33:23 +0700

    menambahkan implementasi dengan state-based construction

--- Program.cs
+++ Program.cs
@@ -1,16 +1,81 @@
 ﻿using System;
+using static DoorMachine;
 
-public class kodePos
+//public class kodePos
+//{
+//    public enum Kelurahan
+//    {
+//        Batununggal, Kujangsari, Mengger,
+//        Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru,
+//        Maleer, Samoja
+//    };
+
+//    public static int getKodePos(Kelurahan kelurahan)
+//    {
+//        int[] kodePos = {40266, 40287, 40267, 40256, 40287,
+//        40286, 40286, 40286, 40272, 40274, 40273};
+
+//        return kodePos[(int)kelurahan];
+//    }
+//}
+
+public class DoorMachine
 {
-    public enum kelurahan {Batununggal, Kujangsari, Mengger,
-        Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru,
-        Maleer, Samoja};
+    public enum state {terkunci, terbuka};
 
-    public static int getKodePos (kelurahan klrhn)
+    private state keadaanSekarang;
+
+    public DoorMachine()
     {
-        int[] kodePos = {40266, 40287, 40267, 40256, 40287,
-        40286, 40286, 40286, 40272, 40274, 40273};
+        keadaanSekarang = state.terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
 
-        return kodePos[(int) klrhn];
+    public void BukaPintu()
+    {
+        if (keadaanSekarang == state.terkunci)
+        {
+            keadaanSekarang = state.terbuka;
+            Console.WriteLine("Buka Pintu");
+        }
+        else
+        {
+            Console.WriteLine("Buka Pintu");
+        }
+    }
+
+    public void KunciPintu()
+    {
+        if (keadaanSekarang == state.terbuka)
+        {
+            keadaanSekarang = state.terkunci;
+            Console.WriteLine("Kunci Pintu");
+        }
+        else
+        {
+            Console.WriteLine("Kunci Pintu");
+        }
+    }
+}
+
+class main
+{
+    static void Main()
+    {
+        DoorMachine pintu = new DoorMachine();
+
+        Console.WriteLine("buka atau kunci pintu?");
+        string perintah = Console.ReadLine();
+
+        if (perintah == "buka")
+        {
+            pintu.BukaPintu();
+        } else if (perintah == "kunci")
+        {
+            pintu.KunciPintu();
+        } else
+        {
+            Console.WriteLine("masukan 'buka' atau 'kunci'");
+        }
     }
 }
